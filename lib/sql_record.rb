ca2bd25a49414f38fdce38661f370d98f209dd5a
squeedee <squeedee@gmail.com>
2011-04-07 04:01:32 +0000

    better code sample

--- lib/sql_record.rb
+++ lib/sql_record.rb
@@ -1,65 +1,65 @@
 module SQLRecord
 
-  def initialize(row)
-    @row = row
-  end
+	def initialize(row)
+		@row = row
+	end
 
-  def self.included(base)
-    base.extend ClassMethods
-  end
+	def self.included(base)
+		base.extend ClassMethods
+	end
 
-  module ClassMethods
+	module ClassMethods
 
-    def with_class klass, &block
-      @current_class = klass
-      block.arity == 2 ? yield(self) : self.instance_eval(&block)
-      @current_class = nil
-    end
+		def with_class klass, &block
+			@current_class = klass
+			block.arity == 2 ? yield(self) : self.instance_eval(&block)
+			@current_class = nil
+		end
 
-    def column attribute_name, opts = {}
-      klass = opts[:class] || @current_class || nil
-      raise ArgumentError, 'Either opts[:class] is not defined or you have not specified a with_class block' if klass.nil?
+		def column attribute_name, opts = {}
+			klass = opts[:class] || @current_class || nil
+			raise ArgumentError, 'Either opts[:class] is not defined or you have not specified a with_class block' if klass.nil?
 
-      source_attribute = (opts[:from] || attribute_name).to_s
+			source_attribute = (opts[:from] || attribute_name).to_s
 
-      define_method attribute_name do
-        klass.columns_hash[source_attribute].type_cast(@row[attribute_name.to_s])
-      end
+			define_method attribute_name do
+				klass.columns_hash[source_attribute].type_cast(@row[attribute_name.to_s])
+			end
 
-      select_column = "#{klass.table_name}.#{source_attribute}"
-      select_column += " as #{attribute_name}" if opts[:from]
+			# bit mucky, a lot here that feels like it should be a little method of its own
+			select_column = "#{klass.table_name}.#{source_attribute}"
+			select_column += " as #{attribute_name}" if opts[:from]
+			(@sql_select_columns ||= []) << select_column
+		end
 
-      (@sql_select_columns ||= []) << select_column
-    end
+		def query &deferred
+			@query_proc = deferred
+		end
 
-    def query &deferred
-      @query_proc = deferred
-    end
+		def find params={}
+			rows = execute_query params
 
-    def find params={}
-      rows = execute_query params
+			rows.map do |row|
+				new row
+			end
+		end
 
-      rows.map do |row|
-        new row
-      end
-    end
+		protected
 
-    protected
+		def execute_query params={}
+			# does this log?
+			sql = ActiveRecord::Base.send(:sanitize_sql_array, get_query_array(params))
+			ActiveRecord::Base.connection.execute(sql)
+		end
 
-    def execute_query params={}
-      # does this log (hope so)
-      sql = ActiveRecord::Base.send(:sanitize_sql_array, get_query_array(params))
-      ActiveRecord::Base.connection.execute(sql)
-    end
+		def get_query_array(params)
+			if @query_proc.arity == 2
+				@query_proc.call(params, @sql_select_columns.join(", "))
+			else
+				@query_proc.call(params)
+			end
+		end
 
-    def get_query_array(params)
-      if @query_proc.arity == 2
-        @query_proc.call(params, @sql_select_columns.join(", "))
-      else
-        @query_proc.call(params)
-      end
-    end
-
-  end
+	end
 
 end
